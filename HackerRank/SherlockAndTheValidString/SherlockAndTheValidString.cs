4dca02565ae60b00a8535b6ac8559b6dbf33dd79
abhishek.singh1 <abhishek.singh1@bold.com>
2019-07-22 14:23:22 +0530

    sherlock-and-valid-string
    https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign

---
+++ HackerRank/SherlockAndTheValidString/SherlockAndTheValidString.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class SherlockAndTheValidString
+    {
+        public string IsSherlockAndTheValidString(string input)
+        {
+            Dictionary<object,int> CharCountPair = GroupBy(input);
+            List<int> groupCounts = GetGroupCounts(CharCountPair);
+            if (groupCounts.Count() > 2)
+                return "NO";
+            else if(groupCounts.Count() ==1)
+            {
+                return "YES";
+            }
+            else
+            {
+                if (((groupCounts[0] - groupCounts[1]) == 1) || ((groupCounts[0] - groupCounts[1]) == -1))
+                    return "YES";
+                else
+                    return "NO";
+            }
+        }
+        private Dictionary<object,int> GroupBy(string input)
+        {
+            Dictionary<object, int> CharCountPair = new Dictionary<object, int>();
+            foreach (object c1 in input)
+            {
+                int count = 0;
+                foreach (object c2 in input)
+                {
+                    if (c1.Equals(c2))
+                        count++;
+                }
+                if(!CharCountPair.Keys.Contains(c1))
+                    CharCountPair.Add(c1, count);
+            }
+            return CharCountPair;
+        }
+        private List<int> GetGroupCounts(Dictionary<object,int> keyValuePairs)
+        {
+            List<int> counts = new List<int>();
+            foreach (KeyValuePair<object,int> kvp in keyValuePairs)
+            {
+                if(!counts.Contains(kvp.Value))
+                    counts.Add(kvp.Value);
+            }
+            return counts;
+        }
+    }
+}
